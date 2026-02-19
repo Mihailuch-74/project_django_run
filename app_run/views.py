7090c68e976eddcc018b4872b890b47cce84e827
Mihailuch <D1ctators74@yandex.ru>
2026-02-19 11:15:21 +0300

    add api company_details

--- app_run/views.py
+++ app_run/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from django.conf import settings
 
-# Create your views here.
+
+@api_view(["GET"])
+def company_details(request):
+    company_name = settings.СOMPANY_NAME
+    slogan = settings.SLOGAN
+    contacts = settings.CONTACTS
+
+    return Response(
+        {"company_name": company_name, "slogan": slogan, "contacts": contacts}
+    )
+
+
+# class CompanyDetails(APIView):
+#     def get(self, request):
+#         company_name = settings.СOMPANY_NAME
+#         slogan = settings.SLOGAN
+#         contacts = settings.CONTACTS
+#
+#         return Response(
+#             {"company_name": company_name, "slogan": slogan, "contacts": contacts}
+#         )
